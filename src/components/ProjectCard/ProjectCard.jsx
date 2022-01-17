f8a01d26b4b798b9459d51ccc3822c2f1500f128
spell-landon <spell.landon@gmail.com>
2022-01-17 09:00:32 -0600

    Iterate through projects

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './ProjectCard.module.css';
+
+function ProjectCard({ data }) {
+    console.log(data.image)
+  return (
+    <div className={styles.cardContainer}>
+      <img src={data.image} alt={data.name} />
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import styles from './Projects.module.css';
+import projectData from '../../data.json';
 // Project cards
 import RecipeaceCard from '../ProjectCards/Recipeace/RecipeaceCard';
 import JobSearchTrackerCard from '../ProjectCards/JobSearchTracker/JobSearchTrackerCard';
 import FlashCardGameCard from '../ProjectCards/FlashCardGame/FlashCardGameCard';
 // Link
 import { Link } from 'react-router-dom';
-
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 function Projects(props) {
-
+  console.log(projectData);
   return (
-    <div
-      className={styles.projectsContainer}>
+    <div className={styles.projectsContainer}>
       <div className={styles.mainContainer}>
         <h1>Projects</h1>
         <div className={styles.gallery}>
-          <Link to='/recipeace'>
+          {/* <Link to='/recipeace'>
             <RecipeaceCard />
           </Link>
           <Link to='/job-search-tracker'>
@@ -24,7 +24,14 @@ function Projects(props) {
           </Link>
           <Link to='/flash-card'>
             <FlashCardGameCard />
-          </Link>
+          </Link> */}
+          {projectData.map((element, index) => {
+            return (
+              <Link to={`/${element.name}`}>
+                <ProjectCard key={index} data={element} />
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
